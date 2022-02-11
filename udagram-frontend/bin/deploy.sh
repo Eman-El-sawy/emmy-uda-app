204fe839b62f030f54ec40b0e14ea6fcc1f6ee4b
Eman ElSawy <dremanelsawy@gmail.com>
2022-02-11 17:44:14 +0200

    edits

---
+++ udagram-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+# aws s3 cp --recursive --acl public-read ./www $S3_BUCKET_FRONTEND
+aws s3 cp --recursive --acl public-read ./www s3://emmy-udagram/ 
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://uda-api-dev.eba-8gpm3in8.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
